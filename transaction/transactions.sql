68a0b1d2b8513ab10a512823f93c3b9fb152d3f9
d1av <davi4alves@gmail.com>
2023-03-06 19:46:42 -0300

    feat: :sparkles: add transactions

---
+++ transaction/transactions.sql
@@ -0,0 +1,48 @@
+SELECT * FROM CLIENTE_AUX
+
+BEGIN TRANSACTION
+INSERT INTO cliente_AUX(nome_cliente,genero,data_de_nascimento,cpf)
+VALUES
+('Maria Julia','F','1995-05-10','987.054.321-00')
+
+ROLLBACK TRANSACTION
+-----
+----
+SELECT * FROM CLIENTE_AUX
+
+BEGIN TRANSACTION Nome_aqui
+INSERT INTO cliente_AUX(nome_cliente,genero,data_de_nascimento,cpf)
+VALUES
+('Maria Julia','F','1995-05-10','987.054.321-00')
+
+--ROLLBACK TRANSACTION
+--COMMIT TRANSACTION
+
+-----
+-----
+DECLARE @contador INT
+
+BEGIN TRANSACTION T1
+INSERT INTO
+cliente_AUX(nome_cliente,genero,data_de_nascimento,cpf)
+VALUES
+('Ruth Campos','F','1995-05-10','987.154.321-00')
+
+SELECT @contador = COUNT(*)
+FROM cliente_AUX WHERE nome_cliente = 'Ruth Campos'
+
+IF @contador = 1
+	BEGIN
+		COMMIT TRANSACTION T1
+		PRINT 'RUTH CAMPOS CADASTRADA COM SUCESSO'
+	END
+ELSE
+	BEGIN
+	ROLLBACK TRANSACTION T1
+	PRINT 'RUTH CAMPOS JÁ ESTÁ CADASTRADA'
+	END
+
+SELECT * FROM CLIENTE_AUX
+
+--ROLLBACK TRANSACTION
+--COMMIT TRANSACTION
